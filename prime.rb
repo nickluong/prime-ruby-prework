da2dfb815c767c1fb94588d209e2247b29168f4c
Nicholas  Luong <nckluong@gmail.com>
2018-08-13 19:53:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime? (num)
+  if(num > 1)
+    divider = num -1 
+    while divider > 1 
+      if num % divider == 0 
+        false 
+      divider -= 1 
+      end 
+    true
+    end 
+  else 
+    false 
+  end 
+end 
